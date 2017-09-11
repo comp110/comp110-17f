874390fb033a5dd78ef50489cab57b6bca3728ee
Kris Jordan <krisjordan@gmail.com>
2017-09-11 09:37:29 -0400

    Progress on lecture 6 idea.

---
+++ src/lec06-objects/00-sample-objects-app.ts
@@ -0,0 +1,32 @@
+import "introcs";
+
+class WeatherData {
+    station: string;
+    precipitation: number;
+    tempHigh: number;
+    tempLow: number;
+}
+
+let sep4th2017: WeatherData = new WeatherData();
+sep4th2017.station = "USW00013722";
+sep4th2017.precipitation = 0;
+sep4th2017.tempHigh = 87;
+sep4th2017.tempLow = 59;
+
+let sep4th2016: WeatherData = new WeatherData();
+sep4th2016.station = "USW00013722";
+sep4th2016.precipitation = 0;
+sep4th2016.tempHigh = 83;
+sep4th2016.tempLow = 61;
+
+let sep4th2015: WeatherData = new WeatherData();
+sep4th2015.station = "USW00013722";
+sep4th2015.precipitation = 0;
+sep4th2015.tempHigh = 92;
+sep4th2015.tempLow = 68;
+
+let sep4th2014: WeatherData = new WeatherData();
+sep4th2014.station = "USW00013722";
+sep4th2014.precipitation = 1.18;
+sep4th2014.tempHigh = 90;
+sep4th2014.tempLow = 71;
