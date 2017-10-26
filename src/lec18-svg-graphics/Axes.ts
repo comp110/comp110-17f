a79721c1d831dacab373f85420ccd2755ac44606
Kris Jordan <krisjordan@gmail.com>
2017-10-26 10:37:08 -0400

    Lec18 wip2

--- src/lec18-svg-graphics/Axes.ts
+++ src/lec18-svg-graphics/Axes.ts
@@ -17,7 +17,11 @@ export class Axes {
     getShapes(): Group {
         let group: Group = new Group();
 
-        // TODO: Create Axes Rectangles
+        let xAxis: Rectangle = new Rectangle(this.width, 0.1, -this.width / 2, 0);
+        group.add(xAxis);
+    
+        let yAxis: Rectangle = new Rectangle(0.1, this.height, 0, -this.height / 2);
+        group.add(yAxis);
 
         return group;
     }
--- src/lec18-svg-graphics/app-script.ts
+++ src/lec18-svg-graphics/app-script.ts
@@ -11,9 +11,34 @@ import {
 
 import { Axes } from "./Axes";
 
+let svgTag: SVG = new SVG("artboard");
+const RADIANS: number = Math.PI * 2;
+
 function main(): void {
 
-    // TODO
+    let group: Group = new Group();
+    let axes: Axes = new Axes(200, 200);
+    group.add(axes.getShapes());
+
+    let count: number = 25;
+    for (let i: number = 0; i < count; i++) {
+
+        let angle: number = i / (count) * RADIANS;
+        let cx: number = Math.cos(angle) * 20;
+        let cy: number = Math.sin(angle) * 20;
+        let c: Circle = new Circle(5, cx, cy);
+
+        let red: number = 0;
+        let green: number = 1 - i / (count - 1);
+        let blue: number = i / (count - 1);
+        c.fill = new Color(red, green, blue);
+        c.stroke = Stroke.NONE;
+
+        group.add(c);
+
+    }
+
+    svgTag.render(group);
 
 }
 
